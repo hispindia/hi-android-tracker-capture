5fec45275b87193003dd7bd8d1f65f77f29b6820
Nhan Cao <nhancv92@gmail.com>
2017-04-04 23:58:35 +0700

    update gradle

--- app/src/main/java/org/hisp/india/trackercapture/MainApplication.java
+++ app/src/main/java/org/hisp/india/trackercapture/MainApplication.java
@@ -2,9 +2,24 @@ package org.hisp.india.trackercapture;
 
 import android.support.multidex.MultiDexApplication;
 
+import com.logentries.logger.AndroidLogger;
+
+import java.io.IOException;
+
 /**
  * Created by nhancao on 4/4/17.
  */
 
 public class MainApplication extends MultiDexApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        try {
+            AndroidLogger.createInstance(getApplicationContext(),
+                    false, true, false, null, 0, "92565dca-71d0-4a1d-9b53-ec0696eda359", true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
