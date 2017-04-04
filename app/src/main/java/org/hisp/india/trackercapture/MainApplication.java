821efc4a89654b69f3baba493086237e1becb9f1
Nhan Cao <nhancv92@gmail.com>
2017-04-04 22:48:19 +0700

    enable multi dex

---
+++ app/src/main/java/org/hisp/india/trackercapture/MainApplication.java
@@ -0,0 +1,10 @@
+package org.hisp.india.trackercapture;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by nhancao on 4/4/17.
+ */
+
+public class MainApplication extends MultiDexApplication {
+}
