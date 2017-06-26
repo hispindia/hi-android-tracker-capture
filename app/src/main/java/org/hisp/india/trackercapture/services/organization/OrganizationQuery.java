39694665107a0654cd3755bc40bbf1f5c50203c3
Nhan Cao <nhancv92@gmail.com>
2017-06-26 08:58:42 +0700

    update OrganizationQuery

--- app/src/main/java/org/hisp/india/trackercapture/services/organization/OrganizationQuery.java
+++ app/src/main/java/org/hisp/india/trackercapture/services/organization/OrganizationQuery.java
@@ -7,7 +7,10 @@ import org.hisp.india.trackercapture.utils.RealmHelper;
 import java.util.ArrayList;
 import java.util.List;
 
+import io.realm.RealmQuery;
 import io.realm.RealmResults;
+
+import static org.hisp.india.trackercapture.utils.RealmHelper.query;
 
 /**
  * Created by nhancao on 4/24/17.
@@ -15,16 +18,22 @@ import io.realm.RealmResults;
 
 public class OrganizationQuery {
 
+    /**
+     * Get all organization from local of current user
+     */
     public static List<ROrganizationUnit> getUserOrganizations() {
-        return RealmHelper.query(realm -> {
+        return query(realm -> {
             RUser user = realm.where(RUser.class).findFirst();
             if (user == null) return new ArrayList<ROrganizationUnit>();
             return realm.copyFromRealm(user.getOrganizationUnits());
         });
     }
 
+    /**
+     * Get all organization from local
+     */
     public static List<ROrganizationUnit> getAllOrganization() {
-        return RealmHelper.query(realm -> {
+        return query(realm -> {
             RealmResults<ROrganizationUnit> tOrganizationUnits = realm.where(ROrganizationUnit.class).findAll();
             return realm.copyFromRealm(tOrganizationUnits);
         });
@@ -32,6 +41,136 @@ public class OrganizationQuery {
 
     public static void insertOrUpdate(List<ROrganizationUnit> organizationUnitList) {
         RealmHelper.transaction(realm -> realm.insertOrUpdate(organizationUnitList));
+    }
+
+    /**
+     * Check data on local is exist or not
+     */
+    public static boolean isEmpty() {
+        Long count = query(realm -> realm.where(ROrganizationUnit.class).count());
+        return count == null || count <= 0;
+    }
+
+    /**
+     * Get organization from local by parent id
+     */
+    public static List<ROrganizationUnit> getOrgFromLocalByParent(final String parent) {
+        return query(realm -> {
+            RealmResults<ROrganizationUnit> realmResults = realm.where(ROrganizationUnit.class)
+                                                                .equalTo("parent", parent)
+                                                                .findAll();
+            return realm.copyFromRealm(realmResults);
+        });
+    }
+
+    /**
+     * Get all org from local by parentId and skip to lower level
+     */
+    public static List<ROrganizationUnit> getOrgFromLocalByLevel(final String parentId, final int lowerLevel) {
+
+        final List<ROrganizationUnit> empty = new ArrayList<>();
+        final List<String> parentIds = new ArrayList<>();
+
+        final ROrganizationUnit parent = RealmHelper
+                .query(realm -> realm.copyFromRealm(
+                        realm.where(ROrganizationUnit.class).equalTo("id", parentId).findFirst()));
+        if (parent == null) return empty;
+
+        final int topLevel = parent.getLevel() + 1;
+        for (int i = topLevel; i <= lowerLevel; i++) {
+            final int finalI = i;
+            List<ROrganizationUnit> listLevel = RealmHelper
+                    .query(realm -> {
+                        RealmResults<ROrganizationUnit> realmResults;
+                        RealmQuery<ROrganizationUnit> query = realm.where(ROrganizationUnit.class)
+                                                                   .equalTo("level", finalI);
+                        if (finalI == topLevel) {
+                            realmResults = query.equalTo("parent", parentId).findAll();
+                        } else {
+                            realmResults = query.in("parent", parentIds.toArray(new String[0]))
+                                                .findAll();
+                        }
+                        return realm.copyFromRealm(realmResults);
+                    });
+
+            if (listLevel == null) {
+                return empty;
+            } else if (finalI == lowerLevel) {
+                return listLevel;
+            }
+
+            parentIds.clear();
+            for (ROrganizationUnit rOrganisationUnit : listLevel) {
+                parentIds.add(rOrganisationUnit.getId());
+            }
+
+        }
+        return empty;
+
+    }
+
+    /**
+     * Get organization from local by level
+     */
+    public static List<ROrganizationUnit> getOrgFromLocalByLevel(final int level) {
+        return query(realm -> {
+            RealmResults<ROrganizationUnit> realmResults = realm.where(ROrganizationUnit.class)
+                                                                .equalTo("level", level)
+                                                                .findAll();
+            return realm.copyFromRealm(realmResults);
+        });
+    }
+
+    /**
+     * Get organization from local by top level and lower level
+     */
+    public static List<ROrganizationUnit> getOrgFromLocalByLevel(final int topLevel, final int lowerLevel) {
+
+        final List<ROrganizationUnit> empty = new ArrayList<>();
+        final List<String> parentIds = new ArrayList<>();
+
+        for (int i = topLevel; i <= lowerLevel; i++) {
+            final int finalI = i;
+            List<ROrganizationUnit> listLevel = RealmHelper
+                    .query(realm -> {
+                        RealmResults<ROrganizationUnit> realmResults;
+                        RealmQuery<ROrganizationUnit> query = realm.where(ROrganizationUnit.class)
+                                                                   .equalTo("level", finalI);
+                        if (finalI == topLevel) {
+                            realmResults = query.findAll();
+                        } else {
+                            realmResults = query.in("parent", parentIds.toArray(new String[0]))
+                                                .findAll();
+                        }
+                        return realm.copyFromRealm(realmResults);
+                    });
+
+            if (listLevel == null) {
+                return empty;
+            } else if (finalI == lowerLevel) {
+                return listLevel;
+            }
+
+            parentIds.clear();
+            for (ROrganizationUnit rOrganisationUnit : listLevel) {
+                parentIds.add(rOrganisationUnit.getId());
+            }
+
+        }
+        return empty;
+
+    }
+
+    /**
+     * Get organization from local by id
+     */
+    public static List<ROrganizationUnit> getOrganisationUnitID(final String id) {
+        return query(realm -> {
+            RealmResults<ROrganizationUnit> realmResults = realm.where(ROrganizationUnit.class)
+                                                                .equalTo("displayName", id)
+                                                                .findAll();
+            return realm.copyFromRealm(realmResults);
+        });
     }
 
 }
